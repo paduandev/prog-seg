7ba465b0fb08471d1545ce52beef24ca2b6a5ab9
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-11-06 21:41:42 -0300

    exercicio aula11

--- aula11/exercicio/AppCafe.java
+++ aula11/exercicio/AppCafe.java
@@ -7,9 +7,11 @@ public class AppCafe {
         Scanner input = new Scanner(System.in);
         int option;
         int qtdeAgua;
-        MaquinaCafe maquinaCafe = new MaquinaCafe();
+        MaquinaCafe maquinaCafe = new MaquinaCafe(80);
 
         do {
+            System.out.println("Nivel de água: " + maquinaCafe.getAgua() + " ml");
+            System.out.println("Cápsulas usadas: " + maquinaCafe.getReservatorioCapsulas());
             System.out.println("1. abastercer água");
             System.out.println("2. fazer café pequeno");
             System.out.println("3. fazer café médio");
@@ -23,20 +25,31 @@ public class AppCafe {
                 case 1:
                     System.out.print("Informe a quantidade de água(ml): ");
                     qtdeAgua = input.nextInt();
-
+                    maquinaCafe.abastecerAgua(qtdeAgua);
                     break;
                 case 2:
-                    maquinaCafe.fazerCafePequeno();
-
+                    try {
+                        maquinaCafe.fazerCafePequeno();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 3:
-
+                    try {
+                        maquinaCafe.fazerCafeMedio();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 4:
-
+                    try {
+                        maquinaCafe.fazerCafeGrande();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case 5:
-
+                    maquinaCafe.esvaziarReservatorio();
                     break;
                 case 6:
                     System.out.println("Desligando...");
--- aula11/exercicio/MaquinaCafe.java
+++ aula11/exercicio/MaquinaCafe.java
@@ -1,34 +1,76 @@
 package exercicio;
 
 public class MaquinaCafe {
+    public final int MAX_AGUA = 950;
+    public final int AGUA_PEQUENO = 50;
+    public final int AGUA_MEDIO = 80;
+    public final int AGUA_GRANDE = 100;
     private int reservatorioCapsulas;
     private int agua;
 
     public MaquinaCafe() {
-
     }
 
     public MaquinaCafe(int agua) {
         this.agua = agua;
     }
 
-    public void abastecerAgua(int agua) {
+    public int getAgua() {
+        return agua;
+    }
 
+    public int getReservatorioCapsulas() {
+        return reservatorioCapsulas;
+    }
+
+    public void abastecerAgua(int agua) {
+        if (agua > 0) {
+            if (this.agua + agua > MAX_AGUA) {
+                this.agua = MAX_AGUA;
+            } else {
+                this.agua += agua;
+            }
+        }
     }
 
     public void esvaziarReservatorio() {
-
+        reservatorioCapsulas = 0;
     }
 
-    public void fazerCafePequeno() {
+    public void fazerCafePequeno() throws Exception {
+        if (reservatorioCapsulas == 4) {
+            throw new Exception("Reservatório de cápsulas cheio");
+        }
+        if (agua < AGUA_PEQUENO) {
+            throw new Exception("Água insificiente");
+        }
 
+        agua -= AGUA_PEQUENO;
+        reservatorioCapsulas++;
     }
 
-    public void fazerCafeMedio() {
+    public void fazerCafeMedio() throws Exception {
+        if (reservatorioCapsulas == 4) {
+            throw new Exception("Reservatório de cápsulas cheio");
+        }
+        if (agua < AGUA_MEDIO) {
+            throw new Exception("Água insificiente");
+        }
 
+        agua -= AGUA_MEDIO;
+        reservatorioCapsulas++;
     }
 
-    public void fazerCafeGrande() {
+    public void fazerCafeGrande() throws Exception {
+        if (reservatorioCapsulas == 4) {
+            throw new Exception("Reservatório de cápsulas cheio");
+        }
+        if (agua < AGUA_GRANDE) {
+            throw new Exception("Água insificiente");
+        }
+
+        agua -= AGUA_GRANDE;
+        reservatorioCapsulas++;
 
     }
 
